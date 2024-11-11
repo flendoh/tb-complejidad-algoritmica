f9ce522f1062b05dd1243557828d3fe8d4710523
ASUS <jhuniortaquiri23@gmail.com>
2024-11-10 23:17:47 -0500

    añadido BFS

--- App.py
+++ App.py
@@ -4,9 +4,11 @@ dpg.create_context()
 from themes.theme import *
 from themes.font import *
 
+
 VACIO = 0
 JUGADOR_1 = 1
 JUGADOR_2 = 2
+WIN_CONDITION_COUNT = 0  
 
 class App:
     def __init__(self):
@@ -14,24 +16,28 @@ class App:
         dpg.bind_theme(global_theme)
         dpg.bind_font(global_font)
         dpg.setup_dearpygui()
+        
         self.log_window = None
         self.main_window = None
         self.table_window = None
         self.CELL_SIZE = 50
         self.table = []
+        self.turno = JUGADOR_1
+
         self.main_window_setup()
         dpg.show_viewport()
         dpg.set_viewport_max_height(600)
         dpg.set_viewport_max_width(800)
         dpg.set_viewport_resizable(False)
+        self.restart_game()
         dpg.start_dearpygui()
 
     def add_text_log(self, text):
         dpg.add_text(text, wrap=200, parent=self.log_window)
-    
+
     def draw_piece(self, x, y, player):
         P2_COLOR = [84, 82, 79]
-        P1_COLOR = [226, 226, 226] 
+        P1_COLOR = [226, 226, 226]
         dpg.draw_circle(
             center=[x * self.CELL_SIZE + self.CELL_SIZE // 2, y * self.CELL_SIZE + self.CELL_SIZE // 2],
             radius=self.CELL_SIZE // 3,
@@ -54,21 +60,21 @@ class App:
         dpg.delete_item(self.log_window, children_only=True)
         self.update_board()
         self.jugar_damas()
-    
+
     def draw_table(self):
         TABLE_SIZE = 8
         for i in range(TABLE_SIZE):
             for j in range(TABLE_SIZE):
-                color = [255, 255, 255] if (i + j) % 2 == 0 else [50,50,50]
+                color = [255, 255, 255] if (i + j) % 2 == 0 else [50, 50, 50]
                 dpg.draw_rectangle(
                     pmin=[j * self.CELL_SIZE, i * self.CELL_SIZE],
                     pmax=[(j + 1) * self.CELL_SIZE, (i + 1) * self.CELL_SIZE],
                     thickness=0,
-                    color=[50,50,50],
+                    color=[50, 50, 50],
                     fill=color,
                     parent=self.table_window
                 )
-    
+
     def update_board(self):
         self.draw_table()
         for i in range(8):
@@ -77,7 +83,27 @@ class App:
                     self.draw_piece(j, i, JUGADOR_1)
                 elif self.table[i][j] == JUGADOR_2:
                     self.draw_piece(j, i, JUGADOR_2)
-    
+
+    def draw_highlight(self, pos1, pos2):
+        """Resalta las posiciones seleccionadas en el tablero."""
+        self.update_board()
+        color = [255, 0, 0, 255]
+        thickness = 3
+        dpg.draw_rectangle(
+            pmin=[pos1[1] * self.CELL_SIZE, pos1[0] * self.CELL_SIZE],
+            pmax=[(pos1[1] + 1) * self.CELL_SIZE, (pos1[0] + 1) * self.CELL_SIZE],
+            color=color,
+            thickness=thickness,
+            parent=self.table_window
+        )
+        dpg.draw_rectangle(
+            pmin=[pos2[1] * self.CELL_SIZE, pos2[0] * self.CELL_SIZE],
+            pmax=[(pos2[1] + 1) * self.CELL_SIZE, (pos2[0] + 1) * self.CELL_SIZE],
+            color=color,
+            thickness=thickness,
+            parent=self.table_window
+        )
+
     def movimientos_posibles(self, jugador):
         movimientos = []
         for i in range(len(self.table)):
@@ -98,25 +124,41 @@ class App:
                                 if 0 <= salto_fila < 8 and 0 <= salto_col < 8 and self.table[salto_fila][salto_col] == VACIO:
                                     movimientos.append(((i, j), (salto_fila, salto_col)))
         return movimientos
-    
+
     def hacer_movimiento(self, movimiento):
         (fila_actual, col_actual), (fila_nueva, col_nueva) = movimiento
         self.table[fila_nueva][col_nueva] = self.table[fila_actual][col_actual]
         self.table[fila_actual][col_actual] = VACIO
-        
+
         # Movimiento de comer
         if abs(fila_nueva - fila_actual) == 2:
             fila_comida = (fila_actual + fila_nueva) // 2
             col_comida = (col_actual + col_nueva) // 2
             self.table[fila_comida][col_comida] = VACIO
-        
+
         self.update_board()
+        self.check_game_end()
+
+    def check_game_end(self):
+        """Verifica si alguno de los jugadores ha ganado."""
+        piezas_j1 = sum(row.count(JUGADOR_1) for row in self.table)
+        piezas_j2 = sum(row.count(JUGADOR_2) for row in self.table)
+        if piezas_j1 <= WIN_CONDITION_COUNT:
+            self.add_text_log("Jugador 2 gana la partida!")
+        elif piezas_j2 <= WIN_CONDITION_COUNT:
+            self.add_text_log("Jugador 1 gana la partida!")
 
     def jugada_bot(self):
+        """Realiza la jugada del bot usando DFS para priorizar capturas."""
         movimientos = self.movimientos_posibles(JUGADOR_2)
         if movimientos:
-            return random.choice(movimientos)
+            return self.dfs_bot(movimientos)
         return None
+
+    def dfs_bot(self, movimientos):
+        """Utiliza DFS para priorizar movimientos de captura."""
+        capturas = [m for m in movimientos if abs(m[1][0] - m[0][0]) == 2]
+        return random.choice(capturas) if capturas else random.choice(movimientos)
 
     def main_window_setup(self):
         TABLE_SIZE = 8
@@ -142,36 +184,15 @@ class App:
                     self.pos2_y = dpg.add_input_int(label="Y", default_value=0, width=100, callback=lambda: self.draw_highlight((dpg.get_value(self.pos1_y), dpg.get_value(self.pos1_x)), (dpg.get_value(self.pos2_y), dpg.get_value(self.pos2_x))))
                     dpg.add_button(label="Mover", width=235, callback=self.realizar_movimiento_jugador)
 
-                with dpg.plot(no_menus=False, no_title=True, no_box_select=True, no_mouse_pos=True, width=525,height=525, equal_aspects=True) as self.table_window:
-                    default_x = dpg.add_plot_axis(axis=0, no_gridlines=True, no_tick_marks=True, no_tick_labels=True,
-                                                label="", lock_min=True)
-                    dpg.set_axis_limits(axis=default_x, ymin=0, ymax=self.CELL_SIZE*TABLE_SIZE)
-                    default_y = dpg.add_plot_axis(axis=1, no_gridlines=True, no_tick_marks=True, no_tick_labels=True,
-                                                label="", lock_min=True)
-                    dpg.set_axis_limits(axis=default_y, ymin=0, ymax=self.CELL_SIZE*TABLE_SIZE)
+                with dpg.plot(no_menus=False, no_title=True, no_box_select=True, no_mouse_pos=True, width=525, height=525, equal_aspects=True) as self.table_window:
+                    default_x = dpg.add_plot_axis(axis=0, no_gridlines=True, no_tick_marks=True, no_tick_labels=True, label="", lock_min=True)
+                    dpg.set_axis_limits(axis=default_x, ymin=0, ymax=self.CELL_SIZE * TABLE_SIZE)
+                    default_y = dpg.add_plot_axis(axis=1, no_gridlines=True, no_tick_marks=True, no_tick_labels=True, label="", lock_min=True)
+                    dpg.set_axis_limits(axis=default_y, ymin=0, ymax=self.CELL_SIZE * TABLE_SIZE)
                     self.restart_game()
                 
         dpg.set_primary_window(self.main_window, True)
-    
-    def draw_highlight(self, pos1, pos2):
-        self.update_board()
-        color = [255, 0, 0, 255]
-        thickness = 3
-        dpg.draw_rectangle(
-            pmin=[pos1[1] * self.CELL_SIZE, pos1[0] * self.CELL_SIZE],
-            pmax=[(pos1[1] + 1) * self.CELL_SIZE, (pos1[0] + 1) * self.CELL_SIZE],
-            color=color,
-            thickness=thickness,
-            parent=self.table_window
-        )
-        dpg.draw_rectangle(
-            pmin=[pos2[1] * self.CELL_SIZE, pos2[0] * self.CELL_SIZE],
-            pmax=[(pos2[1] + 1) * self.CELL_SIZE, (pos2[0] + 1) * self.CELL_SIZE],
-            color=color,
-            thickness=thickness,
-            parent=self.table_window
-        )
-    
+
     def realizar_movimiento_jugador(self):
         pos1_x = dpg.get_value(self.pos1_x)
         pos1_y = dpg.get_value(self.pos1_y)
@@ -180,6 +201,7 @@ class App:
         if self.turno == JUGADOR_1 and self.es_movimiento_valido(pos1_x, pos1_y, pos2_x, pos2_y):
             movimiento = ((pos1_y, pos1_x), (pos2_y, pos2_x))
             self.hacer_movimiento(movimiento)
+            self.draw_highlight((pos1_y, pos1_x), (pos2_y, pos2_x))  # Resalta el movimiento
             if abs(pos2_y - pos1_y) == 2:
                 self.add_text_log(f"Jugador 1 comió una ficha moviendo de ({pos1_x}, {pos1_y}) a ({pos2_x}, {pos2_y})")
             else:
@@ -217,3 +239,4 @@ class App:
 
 if __name__ == "__main__":
     app = App()
+        
